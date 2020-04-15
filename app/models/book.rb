bb13338d5899d6357e306f1b0cca82e70c21e9fe
Vivica <Ma>
2020-04-15 12:36:28 -0400

    added genre checkbox for new book

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,5 +9,15 @@ class Book < ApplicationRecord
 
     has_many :user_books
     has_many :user_profiles, through: :user_books
+    accepts_nested_attributes_for :book_genres
+
+    def book_genres=(book_genres)
+        book_genres.values.each do |book_genre|
+          if book_genre["title"].present?
+            genre = genre.find_or_create_by(book_genre)
+            self.book_genre << genre
+          end
+        end
+      end
 
 end
