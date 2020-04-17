7ee209ad366c29fabf290c8088ec1988374b516a
devmullets <devmullets@gmail.com>
2020-04-17 08:41:21 -0400

    update styling, fix navbar

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,8 @@
 class ReviewsController < ApplicationController
+    
+    skip_before_action :authorized, only: [:index, :show, :create, :new]  # prevents error from new review create
     before_action :find_review, only:[:show, :edit, :update, :destroy]
+    
     def show
     end 
 
@@ -7,6 +10,8 @@ class ReviewsController < ApplicationController
         if params[:book_id]
             @book_id = params[:book_id]
             @review = Review.new
+            @user = UserProfile.find(session[:user_profile_id])
+
         else
             @review = Review.new
         end 
@@ -19,7 +24,7 @@ class ReviewsController < ApplicationController
         if review.valid?
             new_review = Review.order("created_at").last
             reviewed_book = ReviewedBook.create(book_id: new_review.book_id, review_id: new_review.id)
-            redirect_to review
+            redirect_to book_path(new_review.book_id)
         else
             flash[:errors] = @review.errors.full_messages
             redirect_to new_review_path
